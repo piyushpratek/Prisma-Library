260057c780ee82749cf55de9ca172d7c8d49b25d
Piyush Prateek <piyushprateek231996@gmail.com>
2023-04-30 00:37:40 +0530

    wrote many queries with explanation in comment line it is relatable comments just in script.ts -- use mains as main or somehwre main as mains

---
+++ prisma/migrations/20230429181617_test2/migration.sql
@@ -0,0 +1,22 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `userId` on the `userpreference` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[userPreferenceId]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- DropForeignKey
+ALTER TABLE `userpreference` DROP FOREIGN KEY `UserPreference_userId_fkey`;
+
+-- AlterTable
+ALTER TABLE `user` ADD COLUMN `userPreferenceId` VARCHAR(191) NULL,
+    MODIFY `role` ENUM('BASIC', 'EDITOR', 'ADMIN') NOT NULL DEFAULT 'BASIC';
+
+-- AlterTable
+ALTER TABLE `userpreference` DROP COLUMN `userId`;
+
+-- CreateIndex
+CREATE UNIQUE INDEX `User_userPreferenceId_key` ON `User`(`userPreferenceId`);
+
+-- AddForeignKey
+ALTER TABLE `User` ADD CONSTRAINT `User_userPreferenceId_fkey` FOREIGN KEY (`userPreferenceId`) REFERENCES `UserPreference`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
--- script.ts
+++ script.ts
@@ -1,13 +1,141 @@
 import { PrismaClient } from '@prisma/client';
+//normal syntax
 const prisma = new PrismaClient();
 
+//to log all of our query --- for debugging or seeing the log in terminal
+// const prisma = new PrismaClient({ log: ['query'] });
+
+//-------creating a db
+// async function main() {
+//   //...we will write our prisma client queries here
+//   const user = await prisma.user.create({ data: { name: 'Saurav' } });
+//   console.log(user);
+// }
+
+//------- getting the whole saved db
+// async function main() {
+//   const users = await prisma.user.findMany();
+//   console.log(users);
+// }
+
+//------- deleting everything to run the script
+// async function main() {
+//   await prisma.user.deleteMany();
+// }
+
 async function main() {
-  //...we will write our prisma client queries here
-  const user = await prisma.user.create({ data: { name: 'Saurav' } });
+  //everytime it emptys the database before going to next line
+  await prisma.user.deleteMany();
+  //if we save again then it will give the error Unique constraint failed on the constraint: `User_email_key` so we are using above line for this error to clear everytime it runs
+  const user = await prisma.user.create({
+    data: {
+      name: 'Piyush',
+      email: 'piyush@test.com',
+      age: 25,
+      // nested Creations --- with userPreferences -- hwich we sahy is equal to objecgt
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
+    },
+    //use either include or select not both
+    //to run userPreferences
+    // include: {
+    //   userPreference: true,
+    // },
+
+    //can replace include with select
+    select: {
+      name: true,
+      // userPreference: true,
+      userPreference: { select: { id: true } },
+    },
+  });
   console.log(user);
 }
 
-main()
+//createMany = just like create but passing an array of data ,,,,you cannot use selecty class in createMany
+// async function mains() {
+//   const users = await prisma.user.createMany({
+//     data: [
+//       {
+//         name: 'Demon',
+//         email: 'Demon@test.com',
+//         age: 14,
+//       },
+//       {
+//         name: 'BlackDevil',
+//         email: 'BlackDevil@test.com',
+//         age: 18,
+//       },
+//     ],
+//   });
+//   console.log(users);
+// }
+
+//findUnique -----reading of data  --- unique always returns one single user
+async function changetomains() {
+  const user = await prisma.user.findUnique({
+    where: {
+      // email: 'BlackDevil@test.com',
+      //we define @@unique age and name in schema.prisma with so here we need to define it with _
+      age_name: {
+        age: 14,
+        name: 'Demon',
+      },
+    },
+    //we can use include or select in this
+  });
+
+  console.log(user);
+}
+
+//findFirst ----- to search user based on name
+// async function mains() {
+//   const user = await prisma.user.findFirst({
+//     where: {
+//       name: 'BlackDevil',
+//     },
+//   });
+//   console.log(user);
+// }
+
+// findMany --to find many users that matches
+async function mains() {
+  const user = await prisma.user.findMany({
+    where: {
+      name: 'Hunter',
+    },
+    //orderBy: to make an order in ascending or descneding asc/desc
+    orderBy: {
+      // age: 'asc',
+      age: 'desc',
+    },
+    //if we use distinct on name then it will only get the first Hunter with the name ,,,  can also do with name and age-- it will give Hungter with different age now --it wil return which ever has unique name and age because name and age is different for them
+    // distinct: ['name'],
+    // distinct: ['name', 'age'],
+
+    //pagination --more important then distinct is going to be pagination -- pagination is say take and how much we want to return ,,,
+    take: 2,
+    //skip --we can combine take with skip ,, means we are skipping one user and taking the next two user
+    skip: 0,
+  });
+  console.log(user);
+}
+
+// async function mains() {
+//   await prisma.user.createMany({
+//     data: [
+//       {
+//         name: 'Hunter',
+//         age: 21,
+//         email: 'hunter5@test.com',
+//       },
+//     ],
+//   });
+// }
+mains()
   .catch((e) => {
     console.log(e.message);
   })
